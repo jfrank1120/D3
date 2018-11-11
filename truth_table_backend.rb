472607dca1bb3f91b322898f3eb56b2d0052a7e5
Jared Frank <jfrank1120@verizon.net>
2018-11-11 17:55:27 -0500

    Update 11/11 V.1
    -Got page redirection working
    -Values correctly passed
    -Error checking if all params are met
    -Created page_not_found view

--- truth_table_backend.rb
+++ truth_table_backend.rb
@@ -1,15 +1,24 @@
+# Jared Frank - jaf216@pitt.edu - jfrank1120
 require 'sinatra'
 require 'sinatra/reloader'
 
-
 get '/' do
-
-  erb :index, :locals => {  }
+  puts "Page Loaded"
+  erb :index
 end
 
-
+get '/submit' do
+  puts 'Submitting Data to be checked'
+  true_val = params['true_sym']
+  false_val = params['false_sym']
+  size_val = params['size']
+  if (true_val.nil? || false_val.nil? || size_val.nil?)
+    erb :truth_table, :locals => {true_val: true_val, false_val: false_val, size_val: size_val }
+  end
+  erb :error
+end
 
 not_found do
   status 404
-  erb :error
+  erb :page_not_found
 end
