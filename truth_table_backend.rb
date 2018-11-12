aea5455e8e4d8438ee1b4e39fc03706f548b1a96
Jared Frank <jfrank1120@verizon.net>
2018-11-12 03:07:43 -0500

    Correction, now good requirements

--- truth_table_backend.rb
+++ truth_table_backend.rb
@@ -5,23 +5,27 @@ require 'sinatra/reloader'
 # Return true if data is valid else false
 def check_data(true_val, false_val, size_val)
   # Check if size was set
-  if size_val.nil?
+  puts size_val == ''
+  puts size_val.to_i < 2
+  unless size_val == ''
+      puts 'here'
       if size_val.to_i < 2
+        puts 'less than two'
         return true
-       end
+      end
   end
   # Check if true was set
-  if true_val.nil?
+  unless true_val == ''
     if true_val.length > 1
       return true
     end
   end
-  if false_val.nil?
+  unless false_val == ''
     if false_val.length > 1
       return true
     end
   end
-  if true_val.nil? && false_val.nil?
+  unless true_val == '' && false_val == ''
       if true_val == false_val
           return true
       end
@@ -37,6 +41,7 @@ get '/submit' do
   true_val = params['true_sym']
   false_val = params['false_sym']
   size_val = params['size']
+  puts size_val
   if check_data true_val, false_val, size_val
       erb :error
   else
