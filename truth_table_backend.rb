7b1d676ee296c66699703c40d13de0882fd1a17e
Jared Frank <jfrank1120@verizon.net>
2018-11-12 00:32:02 -0500

    Update 11/12 V.1
    -Created all binary string premutations
    -Printing correct coloumns of table
    - Attempting to swap 1's and 0's with user input values

--- truth_table_backend.rb
+++ truth_table_backend.rb
@@ -2,21 +2,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+# Return true if data is valid else false
+def check_data(true_val, false_val, size_val)
+  if true_val.nil? || false_val.nil? || size_val.nil? || size_val.to_i < 2
+    return true
+  elsif true_val.length > 1 || false_val.length > 1
+    return true
+  elsif true_val == false_val
+    return true
+  else
+    return false
+  end
+end
+
 get '/' do
-  puts "Page Loaded"
   erb :index
 end
 
 get '/submit' do
-  puts 'Submitting Data to be checked'
   true_val = params['true_sym']
   false_val = params['false_sym']
   size_val = params['size']
-  unless (true_val.nil? || false_val.nil? || size_val.nil? || size_val.to_i > 2)
+  if check_data true_val, false_val, size_val
       erb :error
   else
-    puts 'Values are valid'
-    puts 'displaying truth table'
     erb :truth_table, :locals => {true_val: true_val, false_val: false_val, size_val: size_val }
   end
 end
